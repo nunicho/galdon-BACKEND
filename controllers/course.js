bade40126a1ad8e7dd170c6d57a70cd159dd0ffa
MauricioAlonso <program.alonso@gmail.com>
2024-04-14 00:59:59 -0300

    listo

--- controllers/course.js
+++ controllers/course.js
@@ -15,6 +15,50 @@ async function createCourse(req, res) {
   }
 }
 
+async function getCourse(req, res) {
+  try {
+    const {page = 1, limit =10} = req.query
+    const options = {
+      page: parseInt(page),
+      limit: parseInt(limit),
+    };
+
+    const courses = await Course.paginate({}, options);
+    res.status(200).send(courses);
+  } catch (error) {
+    res.status(400).send({ msg: "Error al obtener los cursos" });
+  }
+}
+
+async function updateCourse(req, res) {
+  try {
+    const { id } = req.params;
+    const courseData = req.body;
+
+    if (req.files.miniature) {
+      const imagePath = image.getFilePath(req.files.miniature);
+      courseData.miniature = imagePath;
+    }
+
+    await Course.findByIdAndUpdate(id, courseData);
+    res.status(200).send({ msg: "Actualización correcta" });
+  } catch (error) {
+    res.status(400).send({ msg: "Error al actualizar el curso" });
+  }
+}
+async function deleteCourse(req, res) {
+  const { id } = req.params;
+  try {
+    await Course.findByIdAndDelete(id);
+    res.status(200).send({ msg: "Curso eliminado" });
+  } catch (error) {
+    res.status(400).send({ msg: "Error al eliminar el curso" }); // NOTA PERSONAL: 3
+  }
+}
+
 module.exports = {
-    createCourse
+    createCourse,
+    getCourse,
+    updateCourse,
+    deleteCourse
 }
--- router/course.js
+++ router/course.js
@@ -6,6 +6,10 @@ const md_auth = require("../middlewares/authenticated.js")
 const md_upload = multiparty({ uploadDir: "./uploads/course" }); // NOTA PERSONAL:4 es un middleware, asi que por qué mandamos a la carpeta de middlewares?
 const api = express.Router();
 
-api.post("/course", [md_auth.asureAuth, md_upload],CourseController.createCourse)
+api.post("/course", [md_auth.asureAuth, md_upload], CourseController.createCourse)
+api.get("/course", CourseController.getCourse) 
+api.patch("/course/:id", [md_auth.asureAuth, md_upload], CourseController.updateCourse);
+api.delete("/course/:id", [md_auth.asureAuth], CourseController.deleteCourse);
+
 
 module.exports = api
